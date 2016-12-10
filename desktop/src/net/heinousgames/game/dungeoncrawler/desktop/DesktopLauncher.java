f6b8692ca545ad3f98e1e838b1be16632db7a088
TomNeo <TomNeo14@gmail.com>
2016-12-10 17:27:22 -0500

    Initial Commit to git

---
+++ desktop/src/net/heinousgames/game/dungeoncrawler/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.heinousgames.game.dungeoncrawler.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.heinousgames.game.dungeoncrawler.DungeonCrawler;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DungeonCrawler(), config);
+	}
+}
