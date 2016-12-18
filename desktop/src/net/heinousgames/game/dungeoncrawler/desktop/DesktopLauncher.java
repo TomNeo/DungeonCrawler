4e47d6ce8e4a021e9e3fafa9c49ad84b2f7562cc
Steven Hanus <ministeve412@gmail.com>
2016-12-18 13:58:47 -0500

    real hd without padding

--- desktop/src/net/heinousgames/game/dungeoncrawler/desktop/DesktopLauncher.java
+++ desktop/src/net/heinousgames/game/dungeoncrawler/desktop/DesktopLauncher.java
@@ -7,13 +7,12 @@ import net.heinousgames.game.dungeoncrawler.DungeonCrawler;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1740; //1920; // 960 // 240 // 48
-		// 1740
-		// 960
-		config.height = 960; //1080; // 540 // 135 // 27
+		config.width = 1920; // 960 // 240 // 48
+		config.height = 1080; // 540 // 135 // 27
 //		config.width = 1080;
 //		config.height = 1920;
 		new LwjglApplication(new DungeonCrawler(), config);
 
+//		1740, 960
 	}
 }
