f23365bd72b0ccbdb88ee82e5dcbcd7285a072fb
Steven Hanus <ministeve412@gmail.com>
2016-12-29 23:36:53 -0500

    removing unneeded code related to old control scheme

--- core/src/net/heinousgames/game/dungeoncrawler/HeinousRenderer.java
+++ core/src/net/heinousgames/game/dungeoncrawler/HeinousRenderer.java
@@ -3,10 +3,7 @@ package net.heinousgames.game.dungeoncrawler;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-
-import java.util.Map;
 
 /**
  * Created by User on 12/10/2016.
@@ -17,14 +14,7 @@ public class HeinousRenderer extends OrthogonalTiledMapRenderer {
     private DungeonCrawler game;
     private MapBuffer mapBuffer;
 
-    private TextureRegion playerTexture, openSpotTexture;
-
-    public HeinousRenderer(DungeonCrawler game,  TiledMap inputMap) {
-        super(inputMap);
-        this.game = game;
-        playerTexture = new TextureRegion(new Texture("gfx/player3.png"));
-        openSpotTexture = new TextureRegion(new Texture("gfx/arrow3.png"));
-    }
+    private TextureRegion playerTexture;
 
 
     public HeinousRenderer(DungeonCrawler game, MapBuffer mapBuffer, float unitScale){
@@ -32,7 +22,6 @@ public class HeinousRenderer extends OrthogonalTiledMapRenderer {
         this.mapBuffer = mapBuffer;
         this.game = game;
         playerTexture = new TextureRegion(new Texture("gfx/player3.png"));
-        openSpotTexture = new TextureRegion(new Texture("gfx/arrow3.png"));
     }
 
 
@@ -40,37 +29,19 @@ public class HeinousRenderer extends OrthogonalTiledMapRenderer {
      * draw player... duh
      */
     private void renderPlayer() {
-        if(!game.dead) {
+        if (!game.dead) {
             batch.draw(playerTexture, game.player.pos.x, game.player.pos.y, 1, 1);
         }
     }
 
-    public void HeinousRender(Map<String, Boolean> availableMoves){
+    public void HeinousRender(/*Map<String, Boolean> availableMoves*/){
         this.map = mapBuffer.getCurrentMap().map;
         render();
         batch.begin();
         mapBuffer.getCurrentMap().renderBackground((SpriteBatch)batch);
         renderPlayer();
         mapBuffer.getCurrentMap().renderForeground((SpriteBatch)batch);
-        placeOpenSpots(availableMoves);
         batch.end();
-    }
-
-    private void placeOpenSpots(Map<String, Boolean> available) {
-        if(!game.dead) {
-            if (available.get("left")) {
-                batch.draw(openSpotTexture, game.player.pos.x - 1, game.player.pos.y, 1, 1);
-            }
-            if (available.get("right")) {
-                batch.draw(openSpotTexture, game.player.pos.x + 1, game.player.pos.y, 1, 1);
-            }
-            if (available.get("up")) {
-                batch.draw(openSpotTexture, game.player.pos.x, game.player.pos.y + 1, 1, 1);
-            }
-            if (available.get("down")) {
-                batch.draw(openSpotTexture, game.player.pos.x, game.player.pos.y - 1, 1, 1);
-            }
-        }
     }
 
 }
